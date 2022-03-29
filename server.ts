ffaee910f7414c9f116a0935cdc24adebf018958
Nilay Barde <nilaybarde@gmail.com>
2022-03-29 17:02:22 -0400

    Update server.ts

--- server.ts
+++ server.ts
@@ -6,24 +6,24 @@
  *     <li>tuits</li>
  *     <li>likes</li>
  * </ul>
- * 
+ *
  * Connects to a remote MongoDB instance hosted on the Atlas cloud database
  * service
  */
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from "express";
 import CourseController from "./controllers/CourseController";
 import UserController from "./controllers/UserController";
 import TuitController from "./controllers/TuitController";
 import LikeController from "./controllers/LikeController";
 import mongoose from "mongoose";
-var cors = require('cors')
+var cors = require("cors");
 
 // build the connection string
 const PROTOCOL = "mongodb+srv";
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
-const HOST = "cluster0.m8jeh.mongodb.net";
-const DB_NAME = "myFirstDatabase";
+const HOST = "cluster0.apvzt.mongodb.net";
+const DB_NAME = "tuiter-db";
 const DB_QUERY = "retryWrites=true&w=majority";
 const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
 // connect to the database
@@ -33,11 +33,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) =>
-    res.send('Welcome!'));
+app.get("/", (req: Request, res: Response) => res.send("Welcome!"));
 
-app.get('/add/:a/:b', (req: Request, res: Response) =>
-    res.send(req.params.a + req.params.b));
+app.get("/add/:a/:b", (req: Request, res: Response) =>
+    res.send(req.params.a + req.params.b)
+);
 
 // create RESTful Web service API
 const courseController = new CourseController(app);
